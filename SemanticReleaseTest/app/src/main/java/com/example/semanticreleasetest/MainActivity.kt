d9d2f3452290d83ba11331e18074ab98ef1da7e9
Bricio Velázquez <30354516+Briciovh@users.noreply.github.com>
2021-09-10 14:25:30 -0500

    feat: another change (#4)
    One more change

--- SemanticReleaseTest/app/src/main/java/com/example/semanticreleasetest/MainActivity.kt
+++ SemanticReleaseTest/app/src/main/java/com/example/semanticreleasetest/MainActivity.kt
@@ -8,9 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        feat2()
     }
 
     fun feat2(){
         println("Nice feature")
+        println("One more change")
     }
 }
